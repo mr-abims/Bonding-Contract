59cc2ac80d926e716c0d887f6cbd3c4f0866a075
mr-abims <adebayoabimbola9@gmail.com>
2022-10-10 17:55:27 +0100

    files setup

---
+++ src/Types/Ownable.sol
@@ -0,0 +1,22 @@
+// File contracts/types/Ownable.sol
+
+pragma solidity 0.7.5;
+
+contract Ownable {
+
+    address public policy;
+
+    constructor () {
+        policy = msg.sender;
+    }
+
+    modifier onlyPolicy() {
+        require( policy == msg.sender, "Ownable: caller is not the owner" );
+        _;
+    }
+    
+    function transferManagment(address _newOwner) external onlyPolicy() {
+        require( _newOwner != address(0) );
+        policy = _newOwner;
+    }
+}
