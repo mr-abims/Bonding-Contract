9d0beaf770101f2537b147e9b9c342584701a7ec
mr-abims <adebayoabimbola9@gmail.com>
2022-10-10 18:21:38 +0100

    files setup

---
+++ src/Interface/ITreasury.sol
@@ -0,0 +1,6 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.7.5;
+interface ITreasury {
+    function deposit(address _principleTokenAddress, uint _amountPrincipleToken, uint _amountPayoutToken) external;
+    function valueOfToken( address _principleTokenAddress, uint _amount ) external view returns ( uint value_ );
+}
